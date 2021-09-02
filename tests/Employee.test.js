729826b0e32c95c3bcac5b1cffb794c7789374d6
dMosca <devinmosca@yahoo.com>
2021-09-01 19:42:41 -0600

    Created Employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,8 +1,11 @@
 const Employee = require("../lib/Employee");
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+// const Manager = require("../lib/Manager");
+// const Engineer = require("../lib/Engineer");
+// const Intern = require("../lib/Intern");
 
 describe("Employee Class", () => {
-    it("")
-})
+    it("If parameters are entered correctly, get employee", () => {
+        const devin = new Employee("Devin", "2112", "devin@cooldude.com");
+        console.log(devin);
+    });
+});
